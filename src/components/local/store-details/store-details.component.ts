471a41c59b30af99453bac3aec089666ce976c64
T Rukesh <talapaneni.rukesh@quisitive.com>
2022-08-10 12:07:44 +0530

    changes for login styles, top search nav, and store grid

--- src/components/local/store-details/store-details.component.ts
+++ src/components/local/store-details/store-details.component.ts
@@ -22,9 +22,21 @@ export class StoreDetailsComponent implements OnInit {
     ) {
       var header = document.getElementsByClassName('store_img_fixed')[0];
       header.classList.add('store_img_shrink');
+      var store1Image = document.getElementsByClassName('store-1_img_resolution')[0];
+      store1Image.classList.add('img-after-shrink');
+      var descriptionCard = document.getElementsByClassName('store_description_card')[0];
+      descriptionCard.classList.add('img-after-shrink');
+      var backArrow = document.getElementsByClassName('back-arrow_alignment')[0];
+      backArrow.classList.add('back-arrow_alignment-after-shrink');
     } else {
       var header = document.getElementsByClassName('store_img_fixed')[0];
       header.classList.remove('store_img_shrink');
+      var store1Image = document.getElementsByClassName('store-1_img_resolution')[0];
+      store1Image.classList.remove('img-after-shrink');
+      var descriptionCard = document.getElementsByClassName('store_description_card')[0];
+      descriptionCard.classList.remove('img-after-shrink');
+      var backArrow = document.getElementsByClassName('back-arrow_alignment')[0];
+      backArrow.classList.remove('back-arrow_alignment-after-shrink');
     }
   }
 }
