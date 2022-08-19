257f50e97b838d0b2739e328cf5395f55c36a537
brahmatejapulipati <brahmatejapulipati@gmail.com>
2022-08-20 01:56:21 +0530

    Image shrinking changes

--- src/app/components/local/store-details/store-details.component.ts
+++ src/app/components/local/store-details/store-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-store-details',
@@ -6,37 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./store-details.component.scss'],
 })
 export class StoreDetailsComponent implements OnInit {
+  @ViewChild('storeDetails', { static: true }) storeDetailsRef: ElementRef;
+
   constructor() {}
 
   ngOnInit(): void {
     window.addEventListener('scroll', () => {
-      this.changeImagDiminution();
+      this.transformStoreDetails();
     });
   }
 
-  // while scrolling Img whould be shink
-  changeImagDiminution() {
+  transformStoreDetails(): void {
     if (
       document.body.scrollTop > 50 ||
       document.documentElement.scrollTop > 50
     ) {
-      var header = document.getElementsByClassName('store_img_fixed')[0];
-      header.classList?.add('store_img_shrink');
-      var store1Image = document.getElementsByClassName('store-1_img_resolution')[0];
-      store1Image?.classList.add('img-after-shrink');
-      var descriptionCard = document.getElementsByClassName('store_description_card')[0];
-      descriptionCard.classList?.add('img-after-shrink');
-      var backArrow = document.getElementsByClassName('back-arrow_alignment')[0];
-      backArrow?.classList?.add('back-arrow_alignment-after-shrink');
+      this.storeDetailsRef.nativeElement.style.transform = 'scaleY(.55)';
     } else {
-      var header = document.getElementsByClassName('store_img_fixed')[0];
-      header?.classList?.remove('store_img_shrink');
-      var store1Image = document?.getElementsByClassName('store-1_img_resolution')[0];
-      store1Image?.classList.remove('img-after-shrink');
-      var descriptionCard = document.getElementsByClassName('store_description_card')[0];
-      descriptionCard?.classList?.remove('img-after-shrink');
-      var backArrow = document.getElementsByClassName('back-arrow_alignment')[0];
-      backArrow?.classList?.remove('back-arrow_alignment-after-shrink');
+      this.storeDetailsRef.nativeElement.style.transform = 'scaleY(1)';
     }
   }
 }
