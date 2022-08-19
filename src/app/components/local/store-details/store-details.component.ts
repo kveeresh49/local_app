b6667ce7f08d482ff16fc25112ebc1600597578d
kveeresh49 <koppula.veeresh@gmail.com>
2022-08-19 20:56:15 +0530

    added

--- src/app/components/local/store-details/store-details.component.ts
+++ src/app/components/local/store-details/store-details.component.ts
@@ -21,22 +21,22 @@ export class StoreDetailsComponent implements OnInit {
       document.documentElement.scrollTop > 50
     ) {
       var header = document.getElementsByClassName('store_img_fixed')[0];
-      header.classList.add('store_img_shrink');
+      header.classList?.add('store_img_shrink');
       var store1Image = document.getElementsByClassName('store-1_img_resolution')[0];
-      store1Image.classList.add('img-after-shrink');
+      store1Image?.classList.add('img-after-shrink');
       var descriptionCard = document.getElementsByClassName('store_description_card')[0];
-      descriptionCard.classList.add('img-after-shrink');
+      descriptionCard.classList?.add('img-after-shrink');
       var backArrow = document.getElementsByClassName('back-arrow_alignment')[0];
-      backArrow.classList.add('back-arrow_alignment-after-shrink');
+      backArrow?.classList?.add('back-arrow_alignment-after-shrink');
     } else {
       var header = document.getElementsByClassName('store_img_fixed')[0];
-      header.classList.remove('store_img_shrink');
-      var store1Image = document.getElementsByClassName('store-1_img_resolution')[0];
-      store1Image.classList.remove('img-after-shrink');
+      header?.classList?.remove('store_img_shrink');
+      var store1Image = document?.getElementsByClassName('store-1_img_resolution')[0];
+      store1Image?.classList.remove('img-after-shrink');
       var descriptionCard = document.getElementsByClassName('store_description_card')[0];
-      descriptionCard.classList.remove('img-after-shrink');
+      descriptionCard?.classList?.remove('img-after-shrink');
       var backArrow = document.getElementsByClassName('back-arrow_alignment')[0];
-      backArrow.classList.remove('back-arrow_alignment-after-shrink');
+      backArrow?.classList?.remove('back-arrow_alignment-after-shrink');
     }
   }
 }
