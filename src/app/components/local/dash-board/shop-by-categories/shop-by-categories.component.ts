9adf3ef11871aff9def9db8fb1a6d687eeaa8bc6
T Rukesh <talapaneni.rukesh@quisitive.com>
2022-08-23 14:40:49 +0530

    changes for the shop by categories cards

--- src/app/components/local/dash-board/shop-by-categories/shop-by-categories.component.ts
+++ src/app/components/local/dash-board/shop-by-categories/shop-by-categories.component.ts
@@ -6,11 +6,39 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./shop-by-categories.component.scss']
 })
 export class ShopByCategoriesComponent implements OnInit {
+  startingIndex = 0;
+  endingIndex = 6;
+  totalPages = 0;
+  currentPage = 1;
 
   @Input() categoryCardData :any = [];
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  storeScroll(type:string) {
+    if (type === "next") {
+      this.startingIndex = this.startingIndex + 6;
+      this.endingIndex = this.endingIndex + 6;
+      this.currentPage += 1;
+    }
+    if (type === "prev") {
+      this.startingIndex = this.startingIndex - 6;
+      this.endingIndex = this.endingIndex - 6;
+      this.currentPage -= 1;
+    }
+  }
+  
+  pageSize() {
+    this.totalPages = Math.ceil(this.categoryCardData.length / 5);
+    return this.totalPages;
+  }
+  
+  StartingPage() {
+    this.currentPage = 1;
+    this.startingIndex = 0;
+    this.endingIndex = 6;
   }
 
 }
