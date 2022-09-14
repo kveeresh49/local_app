64693b349fb0252232385dc3298b76b91c130770
kveeresh49 <koppula.veeresh@gmail.com>
2022-09-14 20:01:40 +0530

    changes

--- src/app/shared/components/store-card/store-card.component.ts
+++ src/app/shared/components/store-card/store-card.component.ts
@@ -1,22 +1,24 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-store-card',
   templateUrl: './store-card.component.html',
-  styleUrls: ['./store-card.component.scss']
+  styleUrls: ['./store-card.component.scss'],
 })
 export class StoreCardComponent implements OnInit {
-
-  @Input() nearByStoreData :any;
+  @Input() nearByStoreData: any;
   favoriteIconColor = false;
-  
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+  constructor(private router: Router) {}
 
+  ngOnInit(): void {}
 
   favoriteIconClick() {
-this.favoriteIconColor = !this.favoriteIconColor;
+    this.favoriteIconColor = !this.favoriteIconColor;
+  }
+
+  onCardClick(): void {
+    this.router.navigate(['/store']);
   }
 }
